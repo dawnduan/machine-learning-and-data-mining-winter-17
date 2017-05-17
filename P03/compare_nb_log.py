a681405493a198482acdd667dc8bc9c61476bbc1
Dawn <Dawn@Dawns-MacBook-Air.local>
2017-05-16 21:50:42 -0400

    First commit

---
+++ P03/compare_nb_log.py
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='UTF-8'>
+<meta http-equiv='X-UA-Compatible' content='IE=Edge'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<meta name="csrf-param" content="authenticity_token" />
+<meta name="csrf-token" content="7KbtsNeJfW3HliczQyy3v9+M05V6Xuans/iOLuxM0t6rD/vdET+TRIjFSYObaAL8m8cwUwnEk7w7MgVzPfOEgw==" />
+<link rel="stylesheet" media="all" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600" />
+<link rel="stylesheet" media="all" href="/csc411-2017-01/assets/main.css" />
+<link rel="shortcut icon" type="image/x-icon" href="/csc411-2017-01/assets/favicon.ico" />
+<script>
+//<![CDATA[
+const AUTH_TOKEN = "9e8qVaqq58lWgumfqg7/Sb29mhyvBdF1YEuoOt1p9WCyRjw4bBwJ4BnRhy9ySkoK+fZ52tyfpG7ogSNnDNajPQ==";
+//]]>
+</script>
+
+  <title>MarkUs - login</title>
+  
+</head>
+<body>
+  <script src="/csc411-2017-01/assets/jquery.js"></script>
+<script src="/csc411-2017-01/assets/jquery_ujs.js"></script>
+
+<div class='login'>
+  <div class='login-image'></div>
+
+  <p class='login-course'>
+    CSC411, winter 2017
+  </p>
+
+  <div class='login-error'>
+    
+  </div>
+
+  <div class='login-form'>
+    <form action="/csc411-2017-01/?locale=en" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="fIilSBwNimA4EBanK8KegyVI6PF/ri7A/xg7kzhfG/U7IbMl2rtkSXdDeBfzhivAYQMLNww0W9t30rDO6eBNqA==" />
+      <input type="text" name="user_login" id="user_login" value="" tabindex="1" placeholder="Username" autocorrect="off" autofocus="autofocus" />
+      <input type="password" name="user_password" id="user_password" tabindex="2" placeholder="Password" />
+      <div class='submit'>
+        <input type="submit" name="commit" value="Log in" data-disable-with="Logging in..." tabindex="3" />
+      </div>
+</form>  </div>
+</div>
+
+</body>
+</html>
---
+++ P04/cartpole.py
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset='UTF-8'>
+<meta http-equiv='X-UA-Compatible' content='IE=Edge'>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<meta name="csrf-param" content="authenticity_token" />
+<meta name="csrf-token" content="NFpcvUMw9qlsdN+rvp8svSQ2hV0BU9G7OXSeQnzalvLBTEg1iMTdryZFiPJoUWLAde+CGhydoo++7x8FY5AVIA==" />
+<link rel="stylesheet" media="all" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600" />
+<link rel="stylesheet" media="all" href="/csc411-2017-01/assets/main.css" />
+<link rel="shortcut icon" type="image/x-icon" href="/csc411-2017-01/assets/favicon.ico" />
+<script>
+//<![CDATA[
+const AUTH_TOKEN = "qaYqLJQxeUP1QxM+65KwtFfremxWTYvRlUqwUs3Mqp9csD6kX8VSRb9yRGc9XP7JBjJ9K0uD+OUS0TEV0oYpTQ==";
+//]]>
+</script>
+
+  <title>MarkUs - login</title>
+  
+</head>
+<body>
+  <script src="/csc411-2017-01/assets/jquery.js"></script>
+<script src="/csc411-2017-01/assets/jquery_ujs.js"></script>
+
+<div class='login'>
+  <div class='login-image'></div>
+
+  <p class='login-course'>
+    CSC411, winter 2017
+  </p>
+
+  <div class='login-error'>
+    
+  </div>
+
+  <div class='login-form'>
+    <form action="/csc411-2017-01/?locale=en" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="AXduPC06D65am5CcDhL9GHCrcU6mgosjONLSG0Wt/Wj0YXq05s4kqBCqx8XY3LNlIXJ2CbtM+Be/SVNcWud+ug==" />
+      <input type="text" name="user_login" id="user_login" value="" tabindex="1" placeholder="Username" autocorrect="off" autofocus="autofocus" />
+      <input type="password" name="user_password" id="user_password" tabindex="2" placeholder="Password" />
+      <div class='submit'>
+        <input type="submit" name="commit" value="Log in" data-disable-with="Logging in..." tabindex="3" />
+      </div>
+</form>  </div>
+</div>
+
+</body>
+</html>
