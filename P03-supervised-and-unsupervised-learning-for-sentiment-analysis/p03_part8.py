65e25bafb4b061b878aac4d12834af4da2a205b4
Dawn <dawn7duan@gmail.com>
2017-09-28 12:00:50 -0400

    Update p03_part8.py

--- P03-supervised-and-unsupervised-learning-for-sentiment-analysis/p03_part8.py
+++ P03-supervised-and-unsupervised-learning-for-sentiment-analysis/p03_part8.py
@@ -1,47 +1,44 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset='UTF-8'>
-<meta http-equiv='X-UA-Compatible' content='IE=Edge'>
-<meta name='viewport' content='width=device-width, initial-scale=1'>
-<meta name="csrf-param" content="authenticity_token" />
-<meta name="csrf-token" content="LH5+ViG3Bz44Uz9E24V7qg9ANYT6iPydGW00m+1xbS92eQC/5RX/oNMEKugYzOqdKqL1FPBaGgDUIqx9EIOTTQ==" />
-<link rel="stylesheet" media="all" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600" />
-<link rel="stylesheet" media="all" href="/csc411-2017-01/assets/main.css" />
-<link rel="shortcut icon" type="image/x-icon" href="/csc411-2017-01/assets/favicon.ico" />
-<script>
-//<![CDATA[
-const AUTH_TOKEN = "obgwu+Y2SGkm7aOYqkP2Fjsxru9w1wUAYbSI5LfxIxr7v05SIpSw9826tjRpCmchHtNuf3oF452s+xACSgPdeA==";
-//]]>
-</script>
+import numpy as np
+import time
+import pickle as cPickle
+import math
+import os
+from scipy import spatial
 
-  <title>MarkUs - login</title>
-  
-</head>
-<body>
-  <script src="/csc411-2017-01/assets/jquery.js"></script>
-<script src="/csc411-2017-01/assets/jquery_ujs.js"></script>
+M = np.load("embeddings.npz")["emb"]# TODO need trained embeddings
+ind = np.load("embeddings.npz")["word2ind"].flatten()[0]#word indices: len-41524
 
-<div class='login'>
-  <div class='login-image'></div>
+def cosine(a, b): # compute cosine similarity
+    return (1 - spatial.distance.cosine(a, b))
 
-  <p class='login-course'>
-    CSC411, winter 2017
-  </p>
+def part8(M, ind, word): #Top 10 words using cosine similarity
+    ind_desired = list(ind.keys())[list(ind.values()).index(word)] #the index of target word
+    cos_val = [cosine(M[i,:], M[ind_desired,:]) for i in range(M.shape[0]) if i != ind_desired] # using cosine
+    temp = cos_val[:] # make a copy of the lis and sort the cosine values
+    return [ind[cos_val.index(item)] for item in sorted(temp, reverse = True)[:10]]# Top 10 word list
 
-  <div class='login-error'>
+def findfun(a, b): # return embeddings of the sum of two words
+    ind_a = list(ind.keys())[list(ind.values()).index(a)] 
+    ind_b = list(ind.keys())[list(ind.values()).index(b)]
+    return M[ind_a, :] + M[ind_b, :]
     
-  </div>
+def checksimi(a): # returm top3 similar words for the input embeddings
+    cos_val = [cosine(M[i,:], a) for i in range(M.shape[0])] # using cosine
+    temp = cos_val[:]
+    return [ind[cos_val.index(item)] for item in sorted(temp, reverse = True)[:3]]# Top3 word list
 
-  <div class='login-form'>
-    <form action="/csc411-2017-01/?locale=en" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="&#x2713;" /><input type="hidden" name="authenticity_token" value="45cS4V6ym/Ri+aznbbN0PsxcTdEZidxlz/M3EZK/8J+5kGwImhBjaomuuUuu+uUJ6b6NQRNbOvgCvK/3b00O/Q==" />
-      <input type="text" name="user_login" id="user_login" value="" tabindex="1" placeholder="Username" autocorrect="off" autofocus="autofocus" />
-      <input type="password" name="user_password" id="user_password" tabindex="2" placeholder="Password" />
-      <div class='submit'>
-        <input type="submit" name="commit" value="Log in" data-disable-with="Logging in..." tabindex="3" />
-      </div>
-</form>  </div>
-</div>
+def checkeql(a,b):
+    l = checksimi(findfun(a, b)).remove(a)
+    l.remove(b)
+    return l # return most similar word for sum(a. b) of their embeddings
 
-</body>
-</html>
+'''The following are the scripts to run part8'''
+targets = ['story', 'good']
+for w in targets:
+    res = 'The most similar words to ' + w + ' are '
+    for i in range(len(part8(M, ind, w))-1):
+        res += part8(M, ind, w)[i] + ', '
+    print(res+ part8(M, ind, w)[-1] +'.')
+print('word2vec('+checkeql('joy','happy')[0]+') = word2vec(joy) + word2vec(happy)')
+print('word2vec('+checkeql('better','good')[0]+') = word2vec(better) + word2vec(good)')
+
